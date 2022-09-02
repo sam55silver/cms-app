78a90a71a46f1710058d042dfda87f7d90772a59
Sam Silver <sam55silver@gmail.com>
2022-09-02 17:36:09 -0300

    got rid of firebase

--- pages/_app.js
+++ pages/_app.js
@@ -4,27 +4,6 @@ import Navigation from '../components/navigation';
 
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
-import { getFirestore, collection } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
-
-// init config
-import { initializeApp } from 'firebase/app';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyCEORj7_l8J-gsWzYxomW67Wo1YPDvylgA',
-  authDomain: 'cms-app-1a47d.firebaseapp.com',
-  projectId: 'cms-app-1a47d',
-  storageBucket: 'cms-app-1a47d.appspot.com',
-  messagingSenderId: '858556728249',
-  appId: '1:858556728249:web:07bbb8c92badf29e356c68',
-};
-
-initializeApp(firebaseConfig);
-
-const db = getFirestore();
-const fbStorage = getStorage();
-const colRef = collection(db, 'posts');
-
 // Init ApolloClient
 const client = new ApolloClient({
   uri: 'http://localhost:5001/cms-app-1a47d/us-central1/app/graphql',
